f664973e7e1686069a0e1a8a5f9b1cbc46b52009
Hxlixd <scracchato4@gmail.com>
2023-09-17 17:10:06 +0200

    meow

--- src/main/java/me/hxli/openjoinmessage/JoinLeaveListener.java
+++ src/main/java/me/hxli/openjoinmessage/JoinLeaveListener.java
@@ -1,2 +1,43 @@
-package me.hxli.openjoinmessage;public class JoinLeaveListener {
+package me.hxli.openjoinmessage;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinLeaveListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+
+        Player player = e.getPlayer();
+
+        if (player.hasPlayedBefore()) {
+            e.setJoinMessage(ChatColor.DARK_GRAY + "[" +
+                    ChatColor.DARK_AQUA + "+" +
+                    ChatColor.DARK_GRAY + "] " +
+                    ChatColor.BOLD + ChatColor.GRAY + player.getDisplayName());
+        } else {
+            e.setJoinMessage(ChatColor.GREEN + "Welcome " +
+                    ChatColor.BOLD + ChatColor.GRAY + player.getDisplayName() +
+                    ChatColor.GREEN + " to " +
+                    ChatColor.LIGHT_PURPLE + ChatColor.DARK_PURPLE + "Minecraft Server" +
+                    ChatColor.GREEN + "!");
+        }
+
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e) {
+
+        Player player = e.getPlayer();
+
+        e.setQuitMessage(ChatColor.DARK_GRAY + "[" +
+                ChatColor.RED + "-" +
+                ChatColor.DARK_GRAY + "] " +
+                ChatColor.BOLD + ChatColor.GRAY + player.getDisplayName());
+
+    }
 }
--- src/main/java/me/hxli/openjoinmessage/OpenJoinMessage.java
+++ src/main/java/me/hxli/openjoinmessage/OpenJoinMessage.java
@@ -6,12 +6,8 @@ public final class OpenJoinMessage extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
-    }
+        getServer().getPluginManager().registerEvents(new JoinLeaveListener(), this);
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
